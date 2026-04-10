2af32ec317eb1765429a2eeffd19c45f9ff43d43
Jose Flores <josefloresvm@gmail.com>
2026-04-09 23:57:22 -0500

    Feat: windows game Autonomo2.py

--- Autonomo2.py
+++ Autonomo2.py
@@ -1,2 +1,23 @@
 import pygame
 pygame.init()
+
+pantalla = pygame.display.set_mode((1000, 800))
+pygame.display.set_caption("ATARI PONG JOSE FLORES")
+
+ejecutando = True
+
+while ejecutando:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            ejecutando = False
+
+    pantalla.fill((255,255,255))
+
+
+    
+    
+pygame.quit()
+    
+
+
+
