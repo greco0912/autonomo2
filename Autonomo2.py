9058ae968be3bcb912733bee002583ba6c8f842f
Jose Flores <josefloresvm@gmail.com>
2026-04-11 17:28:15 -0500

    feat: Creacion de paletas

--- Autonomo2.py
+++ Autonomo2.py
@@ -6,12 +6,23 @@ pygame.display.set_caption("ATARI PONG JOSE FLORES")
 
 ejecutando = True
 
+paleta = pygame.Rect(50, 300, 10, 100)
+
+paleta2 = pygame.Rect(940, 300, 10, 100)
+
 while ejecutando:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             ejecutando = False
 
-    pantalla.fill((255,255,255))
+    pantalla.fill((255, 255, 255))
+
+    #PALETA IZQUIERDA
+    pygame.draw.rect(pantalla, (0, 0, 0), paleta)
+    #PALETA DERECHA
+    pygame.draw.rect(pantalla, (0, 0, 0), paleta2)
+    pygame.display.flip()
+
 
 
     
